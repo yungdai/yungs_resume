d8c398a153e8922cb3ef2cd2d665e93429d5b260
Yung Dai <yungchidai@gmail.com>
2014-12-07 19:08:02 -0500

    starting the body of the resume page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'resume/index'
 
+  #makes sure that the root directory goes to resume/index.html.erb
+  root 'resume#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
